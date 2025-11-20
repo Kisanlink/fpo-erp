ffeb8b35573dfaeaae4358e24d610145691bde3a
Kaushik <kaushikkomanduri@gmail.com>
2025-11-20 10:35:52 +0530

    chore: removes all the scripts and keeps the structure clean

--- scripts/test-clean.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM Clean test runner that filters out GORM hook warnings for Windows
-
-REM Run tests and use PowerShell to filter warnings
-go test %* 2>&1 | powershell -Command "$input | Where-Object { $_ -notmatch \"don't match Before.*Interface\" -and $_ -notmatch \"\\[3[0-9];1m\" -and $_ -notmatch \"\\[0m\\[35m\" }"
